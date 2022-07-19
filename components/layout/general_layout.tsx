e32fa4943b7a32a4d937edc22cad1b0275f1ca37
ZackarySantana <zackzackyack@gmail.com>
2022-07-19 12:46:29 -0400

    Changed color back to white

--- components/layout/general_layout.tsx
+++ components/layout/general_layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "./general/navbar.tsx";
 
 export default function GeneralLayout(props: { children: ComponentChildren; activePath: string; }) {
     return (
-        <div className={tw`pt-14 bg-onyx h-full min-h-screen text-celeste`}>
+        <div className={tw`pt-14 bg-onyx h-full min-h-screen text-white`}>
             <Navbar activePath={props.activePath} />
             <main>
                 {props.children}
