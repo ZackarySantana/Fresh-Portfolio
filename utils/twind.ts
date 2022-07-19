5129e9da020b3d0a01384a0012b6ecdbe18c481b
ZackarySantana <zackzackyack@gmail.com>
2022-07-19 12:44:37 -0400

    Added custom colors for project

--- utils/twind.ts
+++ utils/twind.ts
@@ -2,7 +2,17 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Configuration, setup } from "twind";
 export * from "twind";
 export const config: Configuration = {
-  darkMode: "class",
-  mode: "silent",
+    darkMode: "class",
+    mode: "silent",
+
+    theme: {
+        extend: {
+            colors: {
+                celeste: "#BAF2E9",
+                cadet: "#5D737E",
+                onyx: "#3F4045",
+            },
+        },
+    },
 };
 if (IS_BROWSER) setup(config);
