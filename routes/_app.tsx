8b925eba6dc66005bcf12d7fb5f36efe6dc627a2
ZackarySantana <zackzackyack@gmail.com>
2022-07-19 19:55:16 -0400

    Added headers

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,8 +8,37 @@ export default function App(props: AppProps) {
     return (
         <>
             <Head>
-                {/* <title>Zackary's Portfolio</title>
-                <meta name="description" content="Hello world!" /> */}
+                <meta
+                    name="keywords"
+                    content="New Grad, Developer, Coder, Programmer, Programming, Languages, Software, Zackary Santana"
+                />
+
+                {/* Other Meta Tags */}
+                <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=5" />
+
+                {/* Manifest */}
+                <link rel="manifest" href="/manifest.json" />
+
+                {/* Images */}
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
             </Head>
             <props.Component />
         </>
