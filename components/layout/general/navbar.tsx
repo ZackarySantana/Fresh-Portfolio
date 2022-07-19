a56bb39a8446c7d7828ca8a4a127500590f46bac
ZackarySantana <zackzackyack@gmail.com>
2022-07-19 12:13:56 -0400

    Added mobile responsiveness

--- components/layout/general/navbar.tsx
+++ components/layout/general/navbar.tsx
@@ -6,20 +6,33 @@ export default function Navbar(props: { activePath: string; }) {
     return (
         <nav className={tw`fixed w-full flex flex-col items-center pt-2 text-gray-200 bg-gray-800 top-0`}>
             <ul className={tw`m-0 list-none`}>
-                <Link to="/" active={props.activePath === "/"}>Home</Link>
-                <Link to="/about" active={props.activePath === "/about"}>About</Link>
-                <Link to="/websites" active={props.activePath === "/websites"}>Webites</Link>
-                <Link to="https://www.linkedin.com/in/zackary-santana/" active={false}>Linkedin</Link>
-                <Link to="/Zackary_Santana_Resume.pdf" active={false}>Resume</Link>
+                {GetLinks().map(l => (
+                    <Link to={l[0]} active={props.activePath.toLowerCase() === l[0]} target={NewTab().includes(l[1])}>{l[1]}</Link>
+                ))}
             </ul>
         </nav>
     );
 }
 
+function GetLinks() {
+    return [
+        ["/", "Home"],
+        ["/about", "About"],
+        ["/projects", "Projects"],
+        ["https://www.linkedin.com/in/zackary-santana/", "Linkedin"],
+        ["/Zackary_Santana_Resume.pdf", "Resume"],
+    ];
+}
+
+function NewTab() {
+    return ["Linkedin", "Resume"];
+}
+
 function Link(props: { children: string; to: string; active: boolean; target?: boolean; }) {
     return (
         <li className={tw`inline-block align-baseline`}>
-            <a href={props.to} target={props.target ? "_blank" : ""} rel="noreferrer" className={tw`h-12 px-4 block leading-10 ${props.active ? "text-red-500 border-b-4 border-current" : ""}`}>
+            <a href={props.to} target={props.target ? "_blank" : ""} rel="noreferrer"
+                className={tw`h-12 block leading-10 !sm:px-4 px-2 sm:text-[100%] text-[85%] ${props.active ? "text-red-500 border-b-4 border-current" : ""}`}>
                 {props.children}
             </a>
         </li>
