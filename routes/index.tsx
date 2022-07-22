cf739779277da1ce6a2de7edaf56f5fada3e3116
ZackarySantana <zackzackyack@gmail.com>
2022-07-21 21:18:00 -0400

    Added some dummy code for toggling

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,6 +2,7 @@
 /** @jsxFrag Fragment */
 import { Fragment, h } from "preact";
 import { tw } from "@twind";
+import { css } from "twind/css";
 import TechCarousel from "../components/TechCarousel.tsx";
 import GeneralLayout from "../components/layout/general_layout.tsx";
 import Link from "../components/utils/link.tsx";
@@ -23,12 +24,21 @@ export default function Home() {
                 <h1 className={tw`sm:text-[5rem] text-[3rem] font-thin uppercase font-josefin`}>Zackary Santana</h1>
                 <p className={tw`my-6 uppercase font-josefin font-bold tracking-[0.125em]`}>A creative full-stack web developer</p>
                 <TechCarousel />
-                <div className={tw`sm:w-[30rem] my-8 w-full`}>
+                <div className={tw`sm:w-[30rem] my-8 w-full  before:content-['hi']`}>
                     <p>
                         Interested in my work? Head over to my <Link to="/projects">my projects</Link> to find out more. I am a passionate developer that has worked with many different languages and frameworks (the carousel does not include everything that makes me great, check out my <ExternalLink to="/Zackary_Santana_Resume.pdf" className={tw`text-blue-400`}>resume</ExternalLink>)
                     </p>
                 </div>
                 <h1 className={tw`text-[2rem]`}>Projects</h1>
+
+                {/* <label for="red-toggle" class={tw`inline-flex relative items-center mr-5 cursor-pointer`}>
+                    <input type="checkbox" value="" id="red-toggle" class={tw`sr-only peer`} checked />
+                    <div class={tw`w-11 h-6 bg-gray-600 rounded-full after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all ${css({
+                        '&::after': { content: '""' },
+                    })}`}></div>
+                    <span class={tw`ml-3 text-sm font-medium text-gray-900 dark:text-gray-300`}>Red</span>
+                </label> */}
+
                 <div className={tw`flex flex-wrap justify-center p-5`}>
                     {getProjects().map(p => (
                         <ProjectCard img={"projects/" + p.img} imgAlt={"projects/" + p.imgAlt} title={p.title} techs={p.techs} desc={p.desc} sub={p.sub} special={p.special} />
@@ -41,7 +51,7 @@ export default function Home() {
                     ))}
                 </div> */}
             </main>
-        </GeneralLayout>
+        </GeneralLayout >
     );
 }
 
