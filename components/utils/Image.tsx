feb23f3b703f0a6607270dcfcba86ae6662e24b4
ZackarySantana <zackzackyack@gmail.com>
2022-07-21 21:46:30 -0400

    Added caching for images

--- components/utils/Image.tsx
+++ components/utils/Image.tsx
@@ -1,11 +1,12 @@
 /** @jsx h */
 import { h } from "preact";
+import { asset } from "$fresh/runtime.ts";
 
 export default function Image(props: { src: string; srcAlt: string; alt: string; id?: string; className?: string; loading?: "eager" | "lazy" | undefined; }) {
     return (
         <picture>
-            <source srcSet={props.src} type="image/webp" />
-            <source srcSet={props.srcAlt} type="image/jpeg" />
+            <source srcSet={asset(props.src)} type="image/webp" />
+            <source srcSet={asset(props.srcAlt)} type="image/jpeg" />
             <img loading={props.loading} src={props.srcAlt} alt={props.alt} id={props.id} className={props.className} />
         </picture>
     );
